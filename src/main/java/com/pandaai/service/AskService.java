4698feff7035b8cfffd5183cb22f1a02ea98ce33
solanacaea <43092492+solanacaea@users.noreply.github.com>
2023-04-24 20:28:31 +0800

    Update AskService.java

--- src/main/java/com/pandaai/service/AskService.java
+++ src/main/java/com/pandaai/service/AskService.java
@@ -32,14 +32,13 @@ public class AskService {
     private WechatService wxService;
 
     public static final int maxLength = 200;
-    public static final String REQUEST_MSG_APPEND = ", please answer above question and return at most 280 characters.";
+//    public static final String REQUEST_MSG_APPEND = ", please answer above question and return at most 280 characters.";
     public static final String SYSTEM_MESSAGE = "你是胖达AI小助手，请回答用户问题，并且每次返回280个字符以内的内容。";
 
     public String ask(Map<String, String> map) {
         try {
             String q = map.get("Content");
             String from = map.get("FromUserName");
-            q += REQUEST_MSG_APPEND;
             Message systemMsg = Message.builder().role(Message.Role.SYSTEM).content(SYSTEM_MESSAGE).build();
             Message message = Message.builder().role(Message.Role.USER).content(q).build();
             List<Message> msgList = Arrays.asList(systemMsg, message);
